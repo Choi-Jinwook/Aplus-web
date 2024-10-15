00e6cba5318ab3bd0a23c0b641052838f26a2677
최진욱 <jinwook.fe@gmail.com>
2024-10-15 17:44:37 +0900

    [feat] queryclient, modalprovider 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
 import { RecoilRoot } from "recoil";
 import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import ModalProvider from "@shared/components/Modal";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 0,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            select: (data: any) => data.data || data,
+          },
+        },
+      }),
+  );
+
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
